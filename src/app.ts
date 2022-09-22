47f30a3239d2f98cb9bc60a08460f2abc8f8893f
Dominik Mrówka <dmrowka7@gmail.com>
2022-09-22 20:54:40 +0200

    add lotto functionality

---
+++ src/app.ts
@@ -0,0 +1,66 @@
+import inquirer from 'inquirer';
+
+
+const chosenNumbers: number[] = [];
+const randomNumbers: number[] = [];
+
+const startApp = async (): Promise<void> => {
+  do {
+    const result = await inquirer.prompt([{
+      name: 'number',
+      type: 'input',
+      message: 'Podaj liczbę...'
+    }]);
+
+    if (validateInput(result.number)) {
+      chosenNumbers.push(parseInt(result.number));
+    }
+  } while (chosenNumbers.length < 6);
+
+  printResult();
+};
+
+const validateInput = (input: string): boolean => {
+  const number = parseInt(input);
+  if (number < 1 || number > 49) {
+    console.log('Choose number from 1 to 49');
+    return false;
+  } else if (chosenNumbers.includes(number)) {
+    console.log('You have already chosen that number');
+    return false;
+  }
+  return true;
+};
+
+const randomNewNumber = (): number => {
+    return Math.floor(Math.random() * 49) + 1;
+};
+
+const validateRandomNumber = (number: number): boolean => {
+  if (randomNumbers.includes(number)) {
+    return false;
+  } else return true;
+};
+
+do {
+    const number: number = randomNewNumber();
+    if (validateRandomNumber(number)) {
+        randomNumbers.push(number);
+    }
+} while (randomNumbers.length < 6);
+
+const checkTheSameNumbers = () => {
+  let result:number = 0;
+  for (let i = 0; i < randomNumbers.length; i++) {
+    if (chosenNumbers.includes(randomNumbers[i])) {
+      result++;
+    }
+  }
+  return result;
+};
+
+const printResult = (): void => {
+    console.log(`You have ${checkTheSameNumbers()} correct numbers`);
+};
+
+startApp();
